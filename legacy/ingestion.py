a9daa24e427a9ceab9d9a78f5d7833067ce0f3e1
Ben Jones <benjones1479@gmail.com>
2025-02-17 19:08:29 +0000

    chore: move legacy code to specific folder

---
+++ legacy/ingestion.py
@@ -0,0 +1,125 @@
+import pandas as pd
+from typing import Iterable, List
+from loguru import logger
+from collections import Counter
+
+DESIRED_COLS = [
+    "season",
+    "Div",
+    "Date",
+    "HomeTeam",
+    "AwayTeam",
+    "B365H",
+    "B365D",
+    "B365A",
+    "FTHG",
+    "FTAG",
+    "FTR",
+    "HTHG",
+    "HTAG",
+    "HTR",
+    "HS",
+    "AS",
+    "HST",
+    "AST",
+    "HC",
+    "AC",
+    "HF",
+    "AF",
+    "HY",
+    "AY",
+    "HR",
+    "AR",
+]
+LEAGUES = [
+    "E0",
+    # "E1",
+    # "E2",
+    # "E3",
+    # "EC",
+    # "SC0",
+    # "SC1",
+    # "SC2",
+    # "SC3",
+    # "D1",
+    # "D2",
+    # "I1",
+    # "I2",
+    # "SP1",
+    # "SP2",
+    # "F1",
+    # "F2",
+]
+
+
+def enrich_data(data: pd.DataFrame) -> pd.DataFrame:
+    output_data = data.copy()
+
+    teams = list(output_data["hometeam"].drop_duplicates())
+    half_way_point = len(teams) - 1
+
+    team_counter = Counter()
+    game_num_h = []
+    game_num_a = []
+    for _, row in output_data.iterrows():
+        team_counter[row["hometeam"]] += 1
+        game_num_h.append(team_counter[row["hometeam"]])
+        team_counter[row["awayteam"]] += 1
+        game_num_a.append(team_counter[row["awayteam"]])
+    output_data["game_num_h"] = game_num_h
+    output_data["game_num_a"] = game_num_a
+
+    output_data["season_half_h"] = 1
+    output_data["season_half_a"] = 1
+    output_data.loc[
+        output_data["game_num_h"] > half_way_point, "season_half_h"
+    ] = 2
+    output_data.loc[
+        output_data["game_num_a"] > half_way_point, "season_half_a"
+    ] = 2
+
+    return output_data
+
+
+def get_data(seasons: Iterable[int], leagues: List[str] | str = "all") -> None:
+    if leagues == "all":
+        leagues = LEAGUES
+    elif not set(leagues).issubset(set(LEAGUES)):
+        raise ValueError(
+            f"""
+            Invalid league provided. Please chose one or more from {LEAGUES}
+        """
+        )
+
+    data_list = []
+    for start_y in seasons:
+        season = str(start_y) + str(start_y + 1)
+        season_tag = str(start_y) + "_" + str(start_y + 1)
+        for league in leagues:
+            url = (
+                "https://www.football-data.co.uk/mmz4281/"
+                + f"{season}/{league}.csv"
+            )
+            try:
+                data = pd.read_csv(url)
+                data["season"] = season_tag
+                data = data[DESIRED_COLS]
+                data.columns = [col.lower() for col in data.columns]
+                processed_data = enrich_data(data)
+                data_list.append(processed_data)
+                logger.info(
+                    f"Sucessfully loaded data for {league} in {season_tag}"
+                )
+            except Exception as e:
+                logger.info(
+                    f"Error loading data for {league} in {season_tag}: {e}"
+                )
+                continue
+
+    data_full = pd.concat(data_list)
+    data_full.to_csv("data/raw_games.csv", index=False)
+    logger.info("All available data loaded and saved to data/raw_games.csv")
+
+
+if __name__ == "__main__":
+    get_data(range(10, 25))
---
+++ legacy/preprocess.py
@@ -0,0 +1,196 @@
+import pandas as pd
+from typing import List
+from loguru import logger
+import os
+from src.helper_functions import get_train_test
+
+
+def create_dataframe(
+    data: pd.DataFrame, exclude_seasons: List[str] = None
+) -> pd.DataFrame:
+
+    data = data.copy()
+    logger.info("Starting dataset creation")
+
+    if exclude_seasons is not None:
+        data = data[~data["season"].isin(exclude_seasons)]
+        logger.info(f"Sucessfully removed seasons: {exclude_seasons}")
+
+    home_h1_data = data.loc[
+        data["season_half_h"] == 1,
+        [
+            "season",
+            "div",
+            "hometeam",
+            "fthg",
+            "hthg",
+            "hs",
+            "hst",
+            "hc",
+            "hf",
+            "hy",
+            "hr",
+            "ftag",
+            "htag",
+            "as",
+            "ast",
+            "ac",
+            "af",
+            "ay",
+            "ar",
+        ],
+    ]
+    away_h1_data = data.loc[
+        data["season_half_a"] == 1,
+        [
+            "season",
+            "div",
+            "awayteam",
+            "ftag",
+            "htag",
+            "as",
+            "ast",
+            "ac",
+            "af",
+            "ay",
+            "ar",
+            "fthg",
+            "hthg",
+            "hs",
+            "hst",
+            "hc",
+            "hf",
+            "hy",
+            "hr",
+        ],
+    ]
+
+    home_h1_data.rename(
+        columns={
+            "hometeam": "team",
+            "fthg": "avg_goals_scored",
+            "hthg": "avg_ht_goals_scored",
+            "hs": "avg_shots",
+            "hst": "avg_sot",
+            "hc": "avg_corners",
+            "hf": "avg_fouls",
+            "hy": "avg_yellows",
+            "hr": "avg_reds",
+            "ftag": "avg_goals_conceded",
+            "htag": "avg_ht_goals_conceded",
+            "as": "avg_shots_faced",
+            "ast": "avg_sot_faced",
+            "ac": "avg_corners_faced",
+            "af": "avg_fouls_against",
+            "ay": "avg_yellows_against",
+            "ar": "avg_reds_against",
+        },
+        inplace=True,
+    )
+
+    away_h1_data.rename(
+        columns={
+            "awayteam": "team",
+            "ftag": "avg_goals_scored",
+            "htag": "avg_ht_goals_scored",
+            "as": "avg_shots",
+            "ast": "avg_sot",
+            "ac": "avg_corners",
+            "af": "avg_fouls",
+            "ay": "avg_yellows",
+            "ar": "avg_reds",
+            "fthg": "avg_goals_conceded",
+            "hthg": "avg_ht_goals_conceded",
+            "hs": "avg_shots_faced",
+            "hst": "avg_sot_faced",
+            "hc": "avg_corners_faced",
+            "hf": "avg_fouls_against",
+            "hy": "avg_yellows_against",
+            "hr": "avg_reds_against",
+        },
+        inplace=True,
+    )
+    logger.info("Home and away dataframes created succesfully")
+
+    all_h1_data = pd.concat([home_h1_data, away_h1_data])
+    stats_h1 = (
+        all_h1_data.groupby(["season", "div", "team"]).mean().reset_index()
+    )
+    logger.info("Home and away dataframes succesfully combined and grouped")
+
+    results_h2 = data.loc[
+        (data["season_half_h"] == 2) & (data["season_half_a"] == 2),
+        [
+            "season",
+            "div",
+            "date",
+            "hometeam",
+            "awayteam",
+            "ftr",
+            "b365h",
+            "b365d",
+            "b365a",
+        ],
+    ]
+    results_h2.rename(
+        columns={"hometeam": "h_team", "awayteam": "a_team"}, inplace=True
+    )
+
+    results_h2 = results_h2.merge(
+        stats_h1.add_prefix("h_"),
+        left_on=["season", "div", "h_team"],
+        right_on=["h_season", "h_div", "h_team"],
+        how="inner",
+    )
+    results_h2 = results_h2.merge(
+        stats_h1.add_prefix("a_"),
+        left_on=["season", "div", "a_team"],
+        right_on=["a_season", "a_div", "a_team"],
+        how="inner",
+    )
+    logger.info("Team stats succesfully merged onto game data")
+
+    results_h2.drop(
+        columns=["h_season", "h_div", "a_season", "a_div"], inplace=True
+    )
+    logger.info("Duplicate columns successfully dropped")
+
+    results_h2["h_win"] = (results_h2["ftr"] == "H").astype(int)
+
+    results_h2.dropna(subset="b365h", inplace=True)
+    results_h2 = results_h2[results_h2["b365h"] != 0]
+
+    bookies_margin = (1 / results_h2["b365h"]) + (1 / results_h2["b365d"]) + (1 / results_h2["b365a"])
+    results_h2["bookies_prob"] = bookies_margin / results_h2["b365h"]
+
+    logger.info("Dataset creation complete")
+    logger.info(f"Dataset length: {len(results_h2)}")
+
+    return results_h2
+
+
+if __name__ == "__main__":
+    raw_games = pd.read_csv("data/raw_games.csv")
+    df = create_dataframe(
+        raw_games, exclude_seasons=["19_20", "20_21", "24_25"]
+    )
+
+    train_val, test = get_train_test(df, test_size=3, random_state=147)
+
+    train_val = train_val.drop(
+        columns=[
+            "div",
+            "date",
+            "h_team",
+            "a_team",
+            "ftr",
+            "b365h",
+            "b365d",
+            "b365a",
+        ]
+    )
+
+    train_val.to_csv("data/train_val_data.csv", index=False)
+    test.to_csv("data/test_data.csv", index=False)
+
+    os.remove("data/raw_games.csv")
