2050939515e0b268871c46ba85b62a49f50f34c7
Ben Jones <benjones1479@gmail.com>
2025-02-22 12:15:21 +0000

    chore: update run_experiment bash file

--- scripts/experimentation/run_experiment.sh
+++ scripts/experimentation/run_experiment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# This file is intended as a quick means of running run_experiment.py
+# Please do not push any changes to this file 
+
 poetry run python src/experiment.py \
     --training_data_path 'data/train_val_data.csv' \
     --learning_rate 0.001 \
     --num_epochs 10000 \
     --num_samples 1000 \
     --num_batches 1 \
-    --league_tag 'epl'
-    # --hidden_units  
+    --league_tag 'epl' \
+    --hidden_units 8
